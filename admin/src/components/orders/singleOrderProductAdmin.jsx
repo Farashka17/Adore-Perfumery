d872cbe1a53d4ae099b53415c17099439834a1da
Farah Veyisli <ferehveyisli@gmail.com>
2024-11-23 21:45:47 +0400

    son

--- admin/src/components/orders/singleOrderProductAdmin.jsx
+++ admin/src/components/orders/singleOrderProductAdmin.jsx
@@ -4,13 +4,17 @@ import Perfume from '../../../../frontend/src/assets/Perfume.svg';
 const SingleOrderProductAdmin = ({ product }) => {
   return (
     <div className="flex gap-12 items-center bg-[#f7f7f6] px-5 py-3">
-      <p className="flex flex-col font-raleway">
+      <p className="flex flex-col font-raleway font-semibold text-[#232323]">
         Name:
-        <span>{product.name}</span> 
+        <span className='font-normal text-[#232323]'>{product.name}</span> 
       </p>
-      <p className="flex flex-col font-raleway">
+      <p className="flex flex-col font-raleway font-semibold text-[#232323]">
         Price:
-        <span>${product.price}</span> 
+        <span className='font-normal text-[#232323]'>${product.price}</span> 
+      </p>
+      <p className="flex flex-col font-raleway font-semibold text-[#232323]">
+        Quantity:
+        <span className='font-normal text-[#232323]'>{product.quantity}</span> 
       </p>
       <div className="w-[111px] h-[111px]">
         <img src={product.productPic || Perfume} className="bg-cover w-[100%] h-[111px]" /> 
